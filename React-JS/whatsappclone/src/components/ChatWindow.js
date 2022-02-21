eca8fc545851a00445f3013d8521d94a839e1747
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-02-20 21:33:12 -0300

    emoji

--- React-JS/whatsappclone/src/components/ChatWindow.js
+++ React-JS/whatsappclone/src/components/ChatWindow.js
@@ -1,41 +1,100 @@
-import React from "react";
-import './ChatWindow.css';
+import React, {useState} from "react";
+import EmojiPicker from "emoji-picker-react";
+import "./ChatWindow.css";
 
 import SearchIcon from "@material-ui/icons/Search";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import CloseIcon from "@material-ui/icons/Close";
+import SendIcon from "@material-ui/icons/Send";
+import MicIcon from "@material-ui/icons/Mic";
 
 export default () => {
-    return (
-        <div className="chatWindow">
-            <div className="chatWindow--header">
 
-                <div className="chatWindow--headerinfo">
-                    <img className="chatWindow--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
-                    <div className="chatWindow--name">Gabriel de Bastiani</div>
-                </div>
+    const [emojiOpen, setEmojiOpen] = useState(false);
 
-                <div className="chatWindow--headerbuttons">
+    const handleEmojiClick = () => {
 
-                    <div className="chatWindow--btn">
-                        <SearchIcon style={{color: '#919191'}} />
-                    </div>
-                    <div className="chatWindow--btn">
-                        <AttachFileIcon style={{color: '#919191'}} />
-                    </div>
-                    <div className="chatWindow--btn">
-                        <MoreVertIcon style={{color: '#919191'}} />
-                    </div>
+    }
 
-                </div>
+    const handleOpenEmoji = () => {
+        setEmojiOpen(true);
+    }
 
-            </div>
-            <div className="chatWindow--body">
+    const handleCloseEmoji = () => {
+        setEmojiOpen(false);
+    }
 
-            </div>
-            <div className="chatWindow--footer">
-
-            </div>
+  return (
+    <div className="chatWindow">
+      <div className="chatWindow--header">
+        <div className="chatWindow--headerinfo">
+          <img
+            className="chatWindow--avatar"
+            src="https://www.w3schools.com/howto/img_avatar2.png"
+            alt=""
+          />
+          <div className="chatWindow--name">Gabriel de Bastiani</div>
         </div>
-    );
-}
+
+        <div className="chatWindow--headerbuttons">
+          <div className="chatWindow--btn">
+            <SearchIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chatWindow--btn">
+            <AttachFileIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chatWindow--btn">
+            <MoreVertIcon style={{ color: "#919191" }} />
+          </div>
+        </div>
+      </div>
+      <div className="chatWindow--body">
+
+      </div>
+
+        <div className="chatWindow--emojiarea" style={{height: emojiOpen ? '200px' : '0px'}}>
+            <EmojiPicker
+                onEmojiClick={handleEmojiClick}
+                disableSkinTonePicker
+                disableSearchBar
+            />
+        </div>
+
+      <div className="chatWindow--footer">
+
+        <div className="chatWindow--pre">
+
+          <div className="chatWindow--btn" onClick={handleCloseEmoji} style={{width: emojiOpen?40:0}}>
+            <CloseIcon style={{ color: "#919191" }} />
+          </div>
+
+          <div className="chatWindow--btn" onClick={handleOpenEmoji}>
+            <InsertEmoticonIcon style={{ color: emojiOpen?'#009688' : "#919191" }} />
+          </div>
+
+        </div>
+        <div className="chatWindow--inputarea">
+            <input
+                className="chatWindow--input"
+                type="text"
+                placeholder="Digite uma mensagem"
+            />
+        </div>
+        <div className="chatWindow--pos">
+
+          <div className="chatWindow--btn">
+            <SendIcon style={{ color: "#919191" }} />
+          </div>
+
+          <div className="chatWindow--btn">
+            <MicIcon style={{ color: "#919191" }} />
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
