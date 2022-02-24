ba61f14ac2d2af670f2c6eeb0abc09f6a18e724f
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-02-23 21:24:20 -0300

    firebase

---
+++ React-JS/whatsappclone/src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDaQDbZ17_tA9WQkkbYsDuI90J9KfT2dh8",
+    authDomain: "whatsappclone-8347d.firebaseapp.com",
+    projectId: "whatsappclone-8347d",
+    storageBucket: "whatsappclone-8347d.appspot.com",
+    messagingSenderId: "906406673041",
+    appId: "1:906406673041:web:e1ab5767729592233e1d30"
+  };
