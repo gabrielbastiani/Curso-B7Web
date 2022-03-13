a871cf19288f33142df272dff739b03b59e5d7dd
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-03-13 17:57:51 -0300

    database

---
+++ Banco-de-dados/src/models/User.ts
@@ -0,0 +1,43 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../instances/mysql';
+
+export interface UserInstance extends Model {
+    id: number;
+    name: string;
+    age: number;
+}
+
+export const User = sequelize.define<UserInstance>("User", {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    },
+    name: {
+        type: DataTypes.STRING,
+    },
+    lastName: {
+        type: DataTypes.STRING
+    },
+    fullName: {
+        type: DataTypes.VIRTUAL,
+        get() {
+            let name: string = this.getDataValue('name');
+            let lastName: string = this.getDataValue('lastName');
+            return `${name} ${lastName}`;
+        }
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue: 18,
+        set(value: number) {
+            if(value < 18) {
+                value = 18;
+            }
+            this.setDataValue('age', value);
+        }
+    }
+}, {
+    tableName: 'users',
+    timestamps: false
+});
